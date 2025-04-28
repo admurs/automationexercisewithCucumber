5bfaf6872ab10efc9f2177cb6227210e0caa5e20
URAS <admurs@gmail.com>
2025-04-28 23:55:01 +0300

    TC20 added and maintenance

--- src/test/java/runners/FailedTestRunner.java
+++ src/test/java/runners/FailedTestRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
-                "html:target/failed-html-report"
+                "html:target/failed-html-report.html"
         },
         features = "@target/rerun.txt",
         glue = "stepdefinitions"
---
+++ src/test/java/stepdefinitions/TC20_SearchProductsandVerifyCartAfterLoginStepDef.java
@@ -0,0 +1,60 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import org.junit.Assert;
+import pages.TC11_VerifySubscriptioninCartPage;
+import pages.TC20_SearchProductsandVerifyCartAfterLoginPage;
+import utilities.BrowserUtils;
+
+public class TC20_SearchProductsandVerifyCartAfterLoginStepDef {
+    TC20_SearchProductsandVerifyCartAfterLoginPage tc20_searchProductsandVerifyCartAfterLoginPage=new TC20_SearchProductsandVerifyCartAfterLoginPage();
+    TC11_VerifySubscriptioninCartPage tc11_verifySubscriptioninCartPage=new TC11_VerifySubscriptioninCartPage();
+    Hooks hooks=new Hooks();
+
+
+    @When("I add those products to cart")
+    public void ıAddThoseProductsToCart() {
+        BrowserUtils.scrollToElement(tc20_searchProductsandVerifyCartAfterLoginPage.searchProductsVerifyText);
+        for (int i = 0; i < tc20_searchProductsandVerifyCartAfterLoginPage.searcProductList.size(); i++) {
+            BrowserUtils.hover(tc20_searchProductsandVerifyCartAfterLoginPage.searcProductList.get(i));
+            tc20_searchProductsandVerifyCartAfterLoginPage.productAddCart.get(i).click();
+            tc20_searchProductsandVerifyCartAfterLoginPage.continueShoppingButton.click();
+        }
+    }
+
+
+    @Then("I verify that {string} are visible in cart")
+    public void ıVerifyThatAreVisibleInCart(String product) {
+        verifyProductCartList(product);
+    }
+
+
+    @When("I click Signup-Login button and submit login details")
+    public void ıClickSignupLoginButtonAndSubmitLoginDetails() {
+        hooks.fillAndCreateAccount();
+        tc20_searchProductsandVerifyCartAfterLoginPage.continueButton.click();
+    }
+
+    @And("I again go to Cart page")
+    public void ıAgainGoToCartPage() {
+        tc11_verifySubscriptioninCartPage.cardPageButton.click();
+    }
+
+    @Then("I verify that those {string} are visible in cart after login as well")
+    public void ıVerifyThatThoseAreVisibleInCartAfterLoginAsWell(String product) {
+        verifyProductCartList(product);
+    }
+
+
+    private void verifyProductCartList(String product)
+    {
+        for (int i = 0; i < tc20_searchProductsandVerifyCartAfterLoginPage.productCartList.size(); i++) {
+            Assert.assertTrue(tc20_searchProductsandVerifyCartAfterLoginPage.productCartList.get(i).getText().contains(product));
+        }
+    }
+
+
+}
